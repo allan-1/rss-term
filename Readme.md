86362d44a2f618f92f80e7b375a769d116410414
Allan Muturi <amuturi01@gmail.com>
2022-02-04 20:26:46 +0300

    initial commit

---
+++ Readme.md
@@ -0,0 +1,25 @@
+# Rss Term
+
+Get rss feeds to your terminal
+
+# Install
+
+`npm install --global rssterm`
+
+# Usage
+
+    $rssterm --help
+
+    usage:
+      rssterm -u [rssurl]
+
+    example:
+      rss -u https://media.rss.com/espionagevr/feed.xml
+
+# Contribution
+
+Contribution and ideas are welcomed.
+
+# License
+
+MIT
---
+++ app.js
@@ -0,0 +1,47 @@
+#!/usr/bin/env node
+
+'use strict';
+const Parser = require('rss-parser');
+const term = require('terminal-kit').terminal;
+const { Command } = require('commander');
+
+const program = new Command();
+const parser = new Parser();
+
+program.name('rssterm').description('Cli to rss utilities').version('1.0.0');
+
+program
+  .argument('<string>', 'rss url')
+  .option('-u, --url <type>', 'Rss url', String);
+
+program.parse(process.argv);
+
+const options = program.opts();
+
+async function rssReader(url) {
+  let feed = await parser.parseURL(url);
+
+  term.green.bold('\n' + feed.title + '\n');
+
+  feed.items.forEach((item) => {
+    term.brightYellow('\n' + item.title + ' : ');
+    term.blue(item.link + '\n');
+    term.grey(item.contentSnippet + '\n');
+  });
+}
+
+// terminate
+function terminate() {
+  term.grabInput(false);
+  setTimeout(function () {
+    process.exit();
+  }, 100);
+}
+
+term.on('key', function (name, matces, data) {
+  if (name === 'CTRL_C') {
+    terminate();
+  }
+});
+
+rssReader(options.url);
